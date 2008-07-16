bcd335aaded7c07b060cd300977007785c431749
knutbo <knutbo@ec38bb0f-9651-0410-8444-2d5d7b445ca6>
2008-07-16 21:49:00 +0000

    asd

---
+++ trunk/BokClient/src/no/knubo/bok/client/util/DelayedServerOracle.java
@@ -0,0 +1,37 @@
+package no.knubo.bok.client.util;
+
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.ui.SuggestOracle;
+
+public abstract class DelayedServerOracle extends SuggestOracle {
+
+	protected Request currentRequest;
+	protected Callback currentCallback;
+	private Timer timer;
+
+	@Override
+	public void requestSuggestions(Request request, Callback callback) {
+		this.currentRequest = request;
+		this.currentCallback = callback;
+
+		startTimer();
+	}
+
+	abstract public void fetchSuggestions();
+
+	private void startTimer() {
+		if (timer == null) {
+			timer = new Timer() {
+
+				@Override
+				public void run() {
+					fetchSuggestions();
+					timer = null;
+				}
+
+			};
+			timer.schedule(500);
+		}
+	}
+
+}
